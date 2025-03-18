53972fe561975b66ed636bd236273439dea18d2f
KeniBeck <deyler_mena69222@elpoli.edu.co>
2025-03-18 17:20:46 -0500

    feat: enhance CalledAttention module with user validation and error handling

--- src/called-attention/called-attention.controller.ts
+++ src/called-attention/called-attention.controller.ts
@@ -1,34 +1,80 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ConflictException,
+  NotFoundException,
+  BadRequestException,
+  UseGuards,
+} from '@nestjs/common';
 import { CalledAttentionService } from './called-attention.service';
 import { CreateCalledAttentionDto } from './dto/create-called-attention.dto';
 import { UpdateCalledAttentionDto } from './dto/update-called-attention.dto';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { ParseIntPipe } from 'src/pipes/parse-int/parse-int.pipe';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('called-attention')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth('access-token')
 export class CalledAttentionController {
-  constructor(private readonly calledAttentionService: CalledAttentionService) {}
+  constructor(
+    private readonly calledAttentionService: CalledAttentionService,
+  ) {}
 
   @Post()
-  create(@Body() createCalledAttentionDto: CreateCalledAttentionDto) {
-    return this.calledAttentionService.create(createCalledAttentionDto);
+  async create(
+    @Body() createCalledAttentionDto: CreateCalledAttentionDto,
+    @CurrentUser('userId') userId: number,
+  ) {
+    createCalledAttentionDto.id_user = userId;
+    const response = await this.calledAttentionService.create(
+      createCalledAttentionDto,
+    );
+    if (response['status'] === 409) {
+      throw new ConflictException(response['message']);
+    } else if (response['status'] === 404) {
+      throw new NotFoundException(response['message']);
+    }else if (response['status'] === 400) {
+      throw new BadRequestException(response['message']);
+    }
+    return response;
   }
 
   @Get()
-  findAll() {
-    return this.calledAttentionService.findAll();
+  async findAll() {
+    const response = await this.calledAttentionService.findAll();
+    return response;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.calledAttentionService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const response = await this.calledAttentionService.findOne(id);
+    return response;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCalledAttentionDto: UpdateCalledAttentionDto) {
-    return this.calledAttentionService.update(+id, updateCalledAttentionDto);
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCalledAttentionDto: UpdateCalledAttentionDto,
+    @CurrentUser('userId') userId: number,
+  ) {
+    updateCalledAttentionDto.id_user = userId;
+    const response = await this.calledAttentionService.update(
+      id,
+      updateCalledAttentionDto,
+    );
+    return response;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.calledAttentionService.remove(+id);
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    const response = await this.calledAttentionService.remove(id);
+    return response;
   }
 }
--- src/called-attention/dto/create-called-attention.dto.ts
+++ src/called-attention/dto/create-called-attention.dto.ts
@@ -1,1 +1,24 @@
-export class CreateCalledAttentionDto {}
+import { ApiHideProperty, ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateCalledAttentionDto {
+    @IsString()
+    @ApiProperty({ example: 'Limpieza de tanque' })
+    name: string;
+
+    @ApiProperty({ example: 'Se necesita limpiar el tanque de agua' })
+    @IsString()
+    description: string;
+
+    @ApiHideProperty()
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    id_user?: number;
+
+    @ApiProperty({ example: '21' })
+    @IsNumber()
+    @Type(() => Number)
+    id_worker: number;
+}
