ae7c2c6b70b5889ca711528a88fb68b91d0ab97d
KeniBeck <deyler_mena69222@elpoli.edu.co>
2025-03-21 15:34:06 -0500

    feat: refactor main.ts to use NestExpressApplication and update package.json scripts for improved build process

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as express from 'express';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
